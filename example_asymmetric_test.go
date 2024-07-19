843f9bc58d81023b2228f418bb5e0348fdf40506
Péter Szilágyi <peterke@gmail.com>
2024-07-19 16:21:43 +0300

    README: add sections about hasher

--- example_asymmetric_test.go
+++ example_asymmetric_test.go
@@ -32,6 +32,12 @@ func (w *WithdrawalAsym) DefineSSZ(codec *ssz.Codec) {
 		ssz.DecodeStaticBytes(dec, &w.Address) // Field (2) - Address        - 20 bytes
 		ssz.DecodeUint64(dec, &w.Amount)       // Field (3) - Amount         -  8 bytes
 	})
+	codec.DefineHasher(func(has *ssz.Hasher) {
+		ssz.HashUint64(has, w.Index)         // Field (0) - Index          -  8 bytes
+		ssz.HashUint64(has, w.Validator)     // Field (1) - ValidatorIndex -  8 bytes
+		ssz.HashStaticBytes(has, &w.Address) // Field (2) - Address        - 20 bytes
+		ssz.HashUint64(has, w.Amount)        // Field (3) - Amount         -  8 bytes
+	})
 }
 
 func ExampleEncodeAsymmetricObject() {
@@ -39,7 +45,10 @@ func ExampleEncodeAsymmetricObject() {
 	if err := ssz.EncodeToBytes(blob, new(WithdrawalAsym)); err != nil {
 		panic(err)
 	}
-	fmt.Printf("ssz: %#x\n", blob)
+	hash := ssz.HashSequential(new(WithdrawalAsym))
+
+	fmt.Printf("ssz: %#x\nhash: %#x\n", blob, hash)
 	// Output:
 	// ssz: 0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+	// hash: 0xdb56114e00fdd4c1f85c892bf35ac9a89289aaecb1ebd0a96cde606a748b5d71
 }
