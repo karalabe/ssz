afe53d7cac33e7d25f0fad66cac29668c99a1942
Péter Szilágyi <peterke@gmail.com>
2024-07-26 11:40:08 +0300

    tests: pull in basic container tests to cover uint8 (#11)

---
+++ tests/testtypes/consensus-spec-tests/types_basics.go
@@ -0,0 +1,22 @@
+// ssz: Go Simple Serialize (SSZ) codec library
+// Copyright 2024 ssz Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package consensus_spec_tests
+
+import "github.com/prysmaticlabs/go-bitfield"
+
+//go:generate go run ../../../cmd/sszgen -type SingleFieldTestStruct -out gen_single_field_test_struct_ssz.go
+//go:generate go run ../../../cmd/sszgen -type BitsStruct -out gen_bits_struct_ssz.go
+
+type SingleFieldTestStruct struct {
+	A byte
+}
+
+type BitsStruct struct {
+	A bitfield.Bitlist `ssz-max:"5"`
+	B [1]byte          `ssz-size:"2" ssz:"bits"`
+	C [1]byte          `ssz-size:"1" ssz:"bits"`
+	D bitfield.Bitlist `ssz-max:"6"`
+	E [1]byte          `ssz-size:"8" ssz:"bits"`
+}
